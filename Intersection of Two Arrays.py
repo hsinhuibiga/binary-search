3cb25d1309d632e8e79a8ce54d268c87d4706c43
hsinhuibiga <hsinhuibiga@gmail.com>
2020-09-05 20:01:09 +0800

    Add files via upload

---
+++ Intersection of Two Arrays.py
@@ -0,0 +1,15 @@
+#Intersection of Two Arrays
+
+class Solution(object):
+    def intersection(self, nums1, nums2):
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :rtype: List[int]
+        """
+        nums1_set = set(nums1)
+        intersect = set()
+        for num in nums2:
+            if num in nums1_set:
+                intersect.add(num)
+        return list(intersect)
