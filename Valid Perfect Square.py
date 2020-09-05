f6684db9de26262d9ba73bdcb0ee8359c420c782
hsinhuibiga <hsinhuibiga@gmail.com>
2020-09-05 19:50:04 +0800

    Add files via upload

---
+++ Valid Perfect Square.py
@@ -0,0 +1,13 @@
+#Valid Perfect Square
+
+class Solution(object):
+    def isPerfectSquare(self, num):
+        """
+        :type num: int
+        :rtype: bool
+        """
+        i = 1
+        while num > 0:
+            num -= i
+            i += 2
+        return num == 0
